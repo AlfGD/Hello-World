d1fac75e7c060c26cc4e6211adeea5faeeaf7515
AlfGD <daniel.guzmandiaz20@gmail.com>
2023-02-17 15:18:16 -0500

    final commit

---
+++ script.js
@@ -0,0 +1,28 @@
+//Variables
+let pokeForm = document.getElementById("form");
+let pokemonGrid = document.querySelector("#pokemon-result");
+let searchButton = document.getElementById("search-btn")
+let resultHeader = document.querySelector(".results-header")
+
+//Event Listeners
+
+pokeForm.addEventListener("submit", (event) =>{
+    event.preventDefault()
+    result = document.querySelector('input').value;
+    page = 1;
+    searchPokemon(result)
+    resultsHeader.textContent = "Result for: " + result
+})
+
+async function searchPokemon(event) {
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${event}`)
+    let responseData = await response.json()
+    console.log(respoonseData)
+}
+
+function displayResults(event) {
+    pokemonGrid.innerHTML += `
+    <div class="pokemon-card">
+    (img class="pokemon-image")" alt="sprite for ${event.name}" src=${event.sprites.front_default}>>
+    </div>`
+}
